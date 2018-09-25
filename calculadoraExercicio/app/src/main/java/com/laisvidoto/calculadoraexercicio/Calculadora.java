16cafee192199accb4930ffb349afdb79fcce32f
laisvidoto1994 <lais.vidoto@gmail.com>
2018-09-25 20:53:09 -0300

    ok

---
+++ calculadoraExercicio/app/src/main/java/com/laisvidoto/calculadoraexercicio/Calculadora.java
@@ -0,0 +1,25 @@
+package com.example.aluno.calculadora;
+
+/**
+ * Created by aluno on 28/08/2018.
+ */
+
+public class Calculadora {
+
+    public static double addition (double value1, double value2){
+        return value1 + value2;
+    }
+
+    public static double subtraction (double value1, double value2){
+        return value1 - value2;
+    }
+    public static double mutliplication (double value1, double value2) {
+        return value1 * value2;
+    }
+
+    public static double division (double value1, double value2) {
+        return value1 / value2;
+    }
+}
+
+
