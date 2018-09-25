e636ec9f35674826b1dedbe488f498cdb0d98266
laisvidoto1994 <lais.vidoto@gmail.com>
2018-09-25 20:54:06 -0300

    add

--- calculadoraExercicio/app/src/main/java/com/laisvidoto/calculadoraexercicio/Calculadora.java
+++ calculadoraExercicio/app/src/main/java/com/laisvidoto/calculadoraexercicio/Calculadora.java
@@ -1,24 +1,29 @@
 package com.example.aluno.calculadora;
 
 /**
- * Created by aluno on 28/08/2018.
+ * Created by aluno on 25/09/2018.
  */
 
 public class Calculadora {
 
-    public static double addition (double value1, double value2){
-        return value1 + value2;
+   public static double adicao(double n1, double n2)
+    {
+        return n1 + n2;
     }
 
-    public static double subtraction (double value1, double value2){
-        return value1 - value2;
-    }
-    public static double mutliplication (double value1, double value2) {
-        return value1 * value2;
+    public static double subtracao(double n1, double n2)
+    {
+        return n1 - n2;
     }
 
-    public static double division (double value1, double value2) {
-        return value1 / value2;
+    public static double divisao(double n1, double n2)
+    {
+        return n1 / n2;
+    }
+
+    public static double multiplicacao(double n1, double n2)
+    {
+        return n1 * n2;
     }
 }
 
